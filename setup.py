b0dc1ce7000f24d5337f9ffca662f9ee3b16c0ea
Alexey Dubkov <alexey.dubkov@elastica.net>
2015-03-13 01:26:37 -0700

    Add py-zabbix dependensy to setup.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 
 setup(name='graphite-to-zabbix',
-      version = '0.1',
+      version = '0.1.1',
       description = 'This tool allow handle alerts based on graphite metrics. It works as a proxy between graphite and zabbix. It use graphite as data source and zabbix as an alerting system.',
       author = 'Alexey Dubkov',
       author_email = 'alexey@dubkov.com',
       url = 'https://github.com/blacked/graphite-to-zabbix',
-      install_requires = ["argparse"],
+      install_requires = ["argparse","py-zabbix"],
       scripts = ['g2zproxy'],
       packages = find_packages(),
      )
