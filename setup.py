9c93ce224ebad133dcf9a66b0382ce722e0e9c4a
Sergey Tuchkin <stuchkin@griddynamics.com>
2015-08-26 17:46:29 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='graphite-to-zabbix',
-      version = '0.1.6',
+      version = '0.2',
       description = 'This tool allow handle alerts based on graphite metrics. It works as a proxy between graphite and zabbix. It use graphite as data source and zabbix as an alerting system.',
       author = 'Alexey Dubkov',
       author_email = 'alexey@dubkov.com',
