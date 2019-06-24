9dbf668ef5721766887d53f4d8bce149236bbbba
Lyr <Lyr@users.noreply.github.com>
2019-06-24 10:58:10 +0200

    Add dependencies pyyaml mandatory

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='graphite-to-zabbix',
       author = 'Alexey Dubkov',
       author_email = 'alexey@dubkov.com',
       url = 'https://github.com/blacked/graphite-to-zabbix',
-      install_requires = ["argparse","py-zabbix"],
+      install_requires = ["argparse","py-zabbix","pyyaml"],
       scripts = ['g2zproxy'],
       packages = find_packages(),
      )
